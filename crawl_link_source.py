356236c1862d3d6f64fbb39c670f8295c5c00c88
lichen <lichen@kangfuzi.cn>
2018-02-26 22:52:24 +0800

    byclass

--- crawl_link_source.py
+++ crawl_link_source.py
@@ -13,7 +13,7 @@ config.set_log('crawl_link_source.log')
 
 def crawl_link_source(link_url, id):
     try:
-        page = handle_urls.handle_url(link_url, 'byid', 'single-content')
+        page = handle_urls.handle_url(link_url, 'byclass', 'image-bookmark')
         if page is not None:
             page = pymysql.escape_string(page)
             update_sql = "update link_urls set page_source='%s',status=1 where id=%s" % (page, id)
